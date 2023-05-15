815bcc0b823f9a91c71ffc494703b4cc8ba57e3c
gummygun <gonzalo.godet@gmail.com>
2023-05-15 12:12:45 -0600

    pre binarySon

--- src/avl.rs
+++ src/avl.rs
@@ -1,9 +1,14 @@
-//use std::ptr;
-use std::ptr::NonNull;
 use arrayvec::ArrayString;
-
-
 use core::{
+    cmp::{
+        Ordering,
+        max
+    },
+    ops::{
+        Index,
+        IndexMut,
+    },
+    ptr::NonNull,
     fmt,
     fmt::{
         Debug,
@@ -11,22 +16,50 @@ use core::{
     },
 };
 
+type Link<T, U> = NonNull<Node<T, U>>;
 
-type Link<T, U> = Option<NonNull<Node<T, U>>>;
+#[derive(Debug, Clone, Copy)]
+enum Side{
+    Left,
+    Right
+}
 
-//#[derive(Debug)]
+struct BinarySon<T>{
+    content:[T; 2]
+}
+
+impl<T> Index<Side> for BinarySon<T>{
+    type Output = T;
+    fn index(&self, index: Side) -> &Self::Output {
+        match index {
+            Side::Left => &self.content[0],
+            Side::Right => &self.content[1],
+        }
+    }
+}
+
+impl<T> IndexMut<Side> for BinarySon<T>{
+    fn index_mut(&mut self, index: Side) -> &mut Self::Output {
+        match index {
+            Side::Left => &mut self.content[0],
+            Side::Right => &mut self.content[1],
+        }
+    }
+}
+
+
 pub struct Map<T:Ord, U>{
-    head: Link<T, U>,
+    head: Option<Link<T, U>>,
     size: u64,
 }
 
-//#[derive(Debug)]
 struct Node<T:Ord, U>{
     index: u64,
     key: T,
     content: U,
-    
-    
+    father: Option<Link<T,U>>,
+    depth: BinarySon<u64>,
+    son: BinarySon<Option<Link<T,U>>>,
 }
 
 pub fn log(){
@@ -43,7 +76,7 @@ pub fn log(){
 }
 
 
-impl<T:Ord, U> Map<T, U>{
+impl<T:Ord+Debug, U:Debug> Map<T, U>{
     pub fn new() -> Self {
         Self{head:None ,size:0}
     }
@@ -53,40 +86,194 @@ impl<T:Ord, U> Map<T, U>{
             key,
             content,
             index:0,
-        })));
-        
-        let _ = new_node.expect("system ran out of memory");
+            father:None,
+            depth: BinarySon{content:[0,0]},
+            son: BinarySon{content:[None,None]},
+        }))).expect("system ran out of memory");
         
         //println!("{:?}", new_node);
-        match self.head.as_ref() {
+        match self.head {
             None => {
-                self.head = new_node;
+                self.head = Some(new_node);
                 self.size = self.size+1;
-                println!("no tiene datos");
+                println!("insert on empty");
                 return false;
             },
             Some(data) => {
-                println!("tiene datos");
-                panic!();
+                insert_node(data, new_node);
+                //println!("{:?}", Node::get_side(new_node));
+                Node::compute_height(new_node);
+                return false;
+                //panic!();
             }
         }
         
     }
+    
 }
+
+fn insert_node<T:Ord+Debug, U:Debug>(mut pivot: Link<T, U>, mut node: Link<T, U>) -> bool{
+    unsafe{
+        loop{
+            let key_order = node.as_ref().key.cmp(&pivot.as_ref().key);
+            let side = match key_order {
+                Ordering::Less => {
+                    Side::Left
+                },
+                Ordering::Equal => {
+                    panic!();
+                },
+                Ordering::Greater => {
+                    Side::Right
+                }
+            };
+            let pivot_mut = pivot.as_mut();
+            match pivot_mut.son[side] {
+                None => {
+                    unsafe {
+                        let node_mut = node.as_mut();
+                        node_mut.father = Some(pivot);
+                    }
+                    pivot_mut.son[side] = Some(node);
+                    //println!("last");
+                    break;
+                },
+                Some(data) => {
+                    pivot = data;
+                    //println!("non last");
+                }
+            }
+            //println!("{:?}", side);
+        }
+        true
+    }
+}
+
+
+fn balance_tree() -> bool {
+    panic!();
+}
+
+fn rotation() -> bool {
+    panic!();
+}
+
+impl<T:Ord+Debug, U:Debug> Node<T, U>{
+    
+    fn compute_height(mut pivot: Link<T, U>) -> bool {
+        println!("computing height-------------------------\n\n");
+        loop{           
+            let side = Node::get_side(pivot);
+            let side = match side {
+                Some(side) => {
+                    side
+                },
+                None => {
+                    /*
+                    codigo cuando es el papa
+                    */
+                    //None::<u8>.expect("es la raiz");
+                    break;
+                }
+            };
+            
+            let mut pivot_father = unsafe {pivot.as_ref().father.unwrap()};
+            let mut pivot_father_mut = unsafe {pivot_father.as_mut()};
+            let pivot_new_depht = Node::get_max_height(pivot)+1;
+            
+            println!("{:?}", side);
+            println!("{:?}", pivot_father_mut);
+            //println!("{:?}", pivot_father);
+            //println!("{:?}", pivot);
+            //println!("{:?}", pivot_new_depht);
+            
+            if pivot_father_mut.depth[side] >= pivot_new_depht {
+                
+                /*
+                dbg!(side);
+                dbg!(pivot_father_mut.depth[side]);
+                dbg!(pivot_new_depht);
+                println!("no hay cambio {:?}", side);
+                */
+                break;
+            }
+            pivot_father_mut.depth[side] = pivot_new_depht;
+            pivot = pivot_father;
+            
+            //println!("{:#?}",pivot_father.as_ref());
+            //println!("{:?}",pivot);
+            //println!("{:#?}",pivot.as_ref());
+            //break;
+        }
+        true
+    }
+    
+    fn get_max_height(node: Link<T, U>)-> u64 {
+        let node_ref = unsafe {node.as_ref()};
+        max(node_ref.depth[Side::Left], node_ref.depth[Side::Right])
+    }
+    
+    fn get_side(node: Link<T, U>) -> Option<Side> {
+        let node_father_ref = unsafe{node.as_ref().father?.as_ref()};
+        if let Some(test) = node_father_ref.son[Side::Left] {
+            if test == node {
+                unsafe {
+                    println!("{:?} {:?} Left", test, node.as_ref());
+                }
+                println!("{:?} {:?} Left", test, node);
+                return Some(Side::Left);
+            }
+        }
+        if let Some(test) = node_father_ref.son[Side::Right] {
+            if test == node {
+                unsafe {
+                    println!("{:?} {:?} Right", test, node.as_ref());
+                }
+                println!("{:?} {:?} Right", test, node);
+                return Some(Side::Right);
+            }
+        }
+        None
+    }
+}
+
+
 
 impl<T:Ord+Debug, U:Debug> Debug for Node<T, U>{
     fn fmt(&self, formater: &mut Formatter) -> fmt::Result {
         match *self {
             Node{
-                index: _,
+                index:_,
+                ref father,
                 ref key,
-                ref content,
-                
+                ref content, 
+                ref depth, 
+                ref son,
             } => {
-            let mut builder = formater.debug_struct("Node");
-            let _ = builder.field("key", &&(*key));
-            let _ = builder.field("key", &&(*content));
-            builder.finish()
+                let mut builder = formater.debug_struct("Node");
+                if let Some(data) = son[Side::Left] {
+                    unsafe {
+                        let _ = builder.field("LeftSon", &&(data.as_ref()));
+                    }
+                    let _ = builder.field("LeftDepth", &&(depth[Side::Left]));
+                }
+                match father {
+                    Some(data) => {
+                        let _ = builder.field("father", &&(*data));
+                    },
+                    None => {
+                        let _ = builder.field("father", &&(*father));
+                    }
+                }
+                let _ = builder.field("key", &&(*key));
+                let _ = builder.field("content", &&(*content));
+                if let Some(data) = son[Side::Right] {
+                    let _ = builder.field("RightDepth", &&(depth[Side::Right]));
+                    unsafe {
+                        let _ = builder.field("RightSon", &&(data.as_ref()));
+                    }
+                }
+                builder.finish()
             }
         }
     }
@@ -99,18 +286,18 @@ impl<T:Ord+Debug, U:Debug> Debug for Map<T, U>{
                 ref size,
                 ref head
             } => {
-            let mut builder = formater.debug_struct("Map");
-            let _ = builder.field("size", &&(*size));
-            match head.as_ref() {
-                None => {
-                    let _ = builder.field("head", &&(*head));
-                    
-                },
-                Some(node) => unsafe {
-                    let _ = builder.field("head", &&((node.as_ref())));
+                let mut builder = formater.debug_struct("Map");
+                let _ = builder.field("size", &&(*size));
+                match head.as_ref() {
+                    None => {
+                        let _ = builder.field("head", &&(*head));
+                        
+                    },
+                    Some(node) => unsafe {
+                        let _ = builder.field("head", &&((node.as_ref())));
+                    }
                 }
-            }
-            builder.finish()
+                builder.finish()
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,19 @@ mod test{
     
     #[test]
     fn test(){
-        avl::log();
+        //avl::log();
         let mut hola = avl::Map::<u64,u64>::new();
+        //println!("{:#?}",hola);
+        hola.add(100, 1);
+        hola.add(50, 0);
+        hola.add(25, 0);
+        println!("\n\n\n\n");
+        hola.add(55, 0);
         println!("{:#?}",hola);
-        hola.add(12, 12);
-        println!("{:#?}",hola);
+        /*
+        */
+        //hola.add(125, 1);
+        //hola.add(25, 1);
+        //println!("{:#?}",hola);
     }
 }
